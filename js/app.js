969fe673109f3672545cc63eccb4a25d5ce7df02
Andrew Brown <alpha121bravo@gmail.com>
2018-10-15 09:44:03 +0100

    some extensions attempted but not working

--- js/app.js
+++ js/app.js
@@ -1,13 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 
+
   const handleFormSubmit = function(event){
     event.preventDefault()
+
+    const findSelection = function(field) {
+      for (pos of position) {
+        if (target.input[pos].checked){
+          return target.input[pos].value;
+        }
+      }
+    }
 
     firstName = event.target.firstName.value;
     lastName = event.target.lastName.value;
     club = event.target.club.value;
     value = event.target.value.value;
+    position = event.findSelection('position');
+
+
+    // radios = event.findSelection('position')
+
+    // const findSelection(field)=function(){
+    //   for (var i = 0, length = radios.length; i < length; i++)
+    //   {if (radios[i].checked){
+    //   return radios[i].value;
+    //   break;
+    //     }
+    //   }
+    // }
+    // var position = findSelection('position')
+    //   const findSelection(field) {
+    //
+    //   }
+
+
 
     const playerCard = document.createElement('div');
 
@@ -19,11 +47,14 @@ document.addEventListener('DOMContentLoaded', () => {
       clubElement.textContent = club;
     const valueElement = document.createElement('p');
       valueElement.textContent = value;
+    const positionElement = document.createElement('p');
+        positionElement.textContent = radios;
 
     playerCard.appendChild(firstNameElement);
     playerCard.appendChild(lastNameElement);
     playerCard.appendChild(clubElement);
     playerCard.appendChild(valueElement);
+    playerCard.appendChild(positionElement);
 
     const list = document.querySelector('#player-list');
     list.appendChild(playerCard);
